938b0c3f2fff5ff3f321353a8e62ab786ea2341a
RyanFlm <info@apelma.de>
2020-10-28 23:38:00 +0100

    Revert and prepare for party

--- src/index.ts
+++ src/index.ts
@@ -7,27 +7,20 @@ import moviesRoute from './ressources/movies';
 import seriesRoute from './ressources/series';
 import usersRoute from './ressources/users';
 
-function wrap(router: express.Router): express.Express {
-    // Create new express instance and apply cors-restriction
-    const app = express();
-    app.use(cors({ origin: true }))
+// Create express instance and apply cors-restriction
+const app = express();
+app.use(cors({ origin: true }))
 
-    // Apply given router
-    app.use("/", router);
+// Apply given routers
+app.use("/media", mediaRoute);
+app.use("/movies", moviesRoute);
+app.use("/series", seriesRoute);
+app.use("/users", usersRoute);
 
-    // Catch all other request to not run into an error
-    app.all('/', (req, res) => response(req, res, 200, 'Welcome to the backend of bergflix! :)'));
-    app.all('*', (req, res) => response(req, res, 404));
+// Catch all other request to not run into an error
+app.all('/', (req, res) => response(req, res, 200, 'Welcome to the backend of bergflix! :)'));
+app.all('*', (req, res) => response(req, res, 404));
 
-    // Return the express instance
-    return app;
-}
 
-// Single exports
-export let mediaApp = wrap(mediaRoute);
-export let moviesApp = wrap(moviesRoute);
-export let seriesApp = wrap(seriesRoute);
-export let usersApp = wrap(usersRoute);
-
-// Default export
-export default wrap(express.Router());
+// Default export app
+export default app;
